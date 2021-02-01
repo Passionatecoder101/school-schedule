39ffc9282816a7250e4848d0721fb901b8900f12
Jackie <76829078+Passionatecoder101@users.noreply.github.com>
2021-02-01 15:18:06 -0500

    Add files via upload

---
+++ schoolSchedule.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+//feel free to change my name to yours!
+
+cout << "Jackie's Schedule\n" << endl;
+
+//first I will add the code for my A Day classes which are on Mondays, and Wedsndays
+
+cout << "A Day\n" << endl;
+
+cout << setw(9) << "Period No:" << setw(12) << "Class" << setw(29) << "Teacher Name" << endl << endl;
+    cout << setw(2) << 1 << setw(23) << "Geometry" << setw(21) << "Rolocut" << endl;
+    cout << setw(2) << 2 << setw(34) << "Ap Computer Science" << setw(15) << "Van Nostrand" << endl;
+    cout << setw(2) << 3 << setw(24) << "Spanish 2" << setw(22) << "Ledbetter" << endl;
+    cout << setw(2) << 4 << setw(34) << "Business Management" << setw(10) << "Shelley" << endl;
+    cout <<"\n" << endl;
+//now I shall add the code for my B Day classes which are on tuesdays and thursdays
+cout << "B Day\n" << endl;
+
+cout << setw(9) << "Period No:" << setw(12) << "Class" << setw(29) << "Teacher Name" << endl << endl;
+    cout << setw(2) << 1 << setw(31) << "Ap World History" << setw(13) << "Lamborn" << endl;
+    cout << setw(2) << 2 << setw(18) << "IPC" << setw(27) << "Henryson" << endl;
+    cout << setw(2) << 3 << setw(25) << "English 10" << setw(17) << "Kates" << endl;
+    cout << setw(2) << 4 << setw(35) << "Drawing and Painting" << setw(10) << "Hendrick" << endl;
+cout << "\n" << endl;
+//now i will add the special things that don't fit in with the above data
+cout << "Extra bits\n" << endl;
+cout << "Lunch: 11:45 - 12:45\n" << endl;
+cout << "Fridays: Asynchronous Learning" << endl;
+int getchar();
+    return 0;
+}
